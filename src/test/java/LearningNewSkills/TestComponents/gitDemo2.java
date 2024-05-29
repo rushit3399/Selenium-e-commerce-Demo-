23364155894d054d8746eb57f08a4c667e620395
Rushit <rushit3399@gmail.com>
2024-05-28 22:14:38 -0600

    commit frm Rushit Demo

---
+++ src/test/java/LearningNewSkills/TestComponents/gitDemo2.java
@@ -0,0 +1,16 @@
+package LearningNewSkills.TestComponents;
+
+import org.testng.annotations.Test;
+
+public class gitDemo2 {
+	
+	@Test
+	public void postJira()
+	{
+		System.out.println("Git Demo 1");
+		System.out.println("Pre Jira");
+		System.out.println("Pre Jira 2");
+		
+	}
+
+}
